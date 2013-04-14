ac9090fbc1526c22051627ac1eed6412d5bd3501
Bryan Paronto <bparonto@gmail.com>
2013-04-14 14:26:36 -0500

    added some config and a Procfile

--- config/env.rb
+++ config/env.rb
@@ -8,7 +8,15 @@ module BP
       configure do
         set :root, File.join(File.expand_path(File.join(File.dirname(__FILE__))), '..')
         set :public_folder, 'public'
+        set :method_override, true
+        set :raise_errors,    false
         set :show_exceptions, false
+        set :sessions,        true
+        set :session_secret,  'b2upc49fbr03n3odhf02nfd78fn'
+        set :erb,             :escape_html => true
+
+        mime_type :ttf, 'font/ttf'
+        mime_type :woff, 'application/octet-stream'
 
         Dir.glob(File.join(root, 'models', '**/*.rb')).each { |f| require f }
 
@@ -18,12 +26,14 @@ module BP
       end
 
       configure :development do
+        use Rack::CommonLogger
         Bundler.require :development 
         DataMapper.setup :default, YAML.load_file(File.join(root, 'config', 'database.yml'))[environment.to_s]
 
       end
 
       configure :test do
+        use Rack::CommonLogger
         Bundler.require :test
         DataMapper.setup :default, YAML.load_file(File.join(root, 'config', 'database.yml'))[environment.to_s]
       end
